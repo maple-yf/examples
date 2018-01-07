b1013f98a5360c758cfc0b8af9ba041cbf53d9f4
MAPLE-PC\maple <maplecumtcs@gmail.com>
2018-01-07 22:36:27 +0800

    修改tags和category 创建webpack简介

---
+++ Webpack-Demo/hello-webpack/js/index.js
@@ -0,0 +1,3 @@
+var data = require("./data");
+
+console.log(data);
---
+++ Webpack-Demo/hello-webpack/webpack.config.js
@@ -0,0 +1,15 @@
+var webpack = require('webpack')
+
+module.exports = {
+    entry: './js/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'dist.js'
+    },
+    module: {
+        loaders: []
+    },
+    plugins: [
+        
+    ]
+}
